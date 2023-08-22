a1c815564eb745b345231d1ee30b7b5896724583
Micael Santana <micaelsantana.dev@gmail.com>
2023-08-22 20:04:26 -0300

    add style css input-text

--- front-end_next/app/Components/Header/header.tsx
+++ front-end_next/app/Components/Header/header.tsx
@@ -5,8 +5,7 @@ const header = () => {
     <header className='bg-gray-50'>
         <div className='xl:container xl:mx-auto flex flex-col items-center sm:flex-row sm:justify-between text-center py-3'>
             <div className='md:flex-none w-96 order-2 sm:order-1 flex justify-center py-4 sm:py-0'>
-                <input type="text" className='mt-1 block w-60 px-3 py-2 bg-white border border-slate-300
-                rounded-full text-sm shadow-sm placeholder-slate-500' placeholder='Search...'/>
+                <input type="text" className='input-text' placeholder='Search...'/>
             </div>
             <div className='shrink w-80 sm:order-2'>
                 <a>Design</a>
