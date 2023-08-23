d6fde1f70f22eb65ec85e48b267bd3566bb7e42f
Micael Santana <micaelsantana.dev@gmail.com>
2023-08-22 21:57:11 -0300

    add paragraphs and component Author

---
+++ front-end_next/app/Components/Child/author.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import Link from "next/link";
+import { ReactNode } from "react";
+
+type AuthorProps = {
+    children: ReactNode;
+};
+
+const Author = ({children}: AuthorProps) => {
+  return (
+    <div className="author flex py-5">
+        <Image src={'/images/author/author1.jpg'} width={80} height={60} alt="" className="rounded-full" />
+        <div className="flex flex-col justify-center px-4">
+        <Link href={'/'}>
+           <span className="text-md font-bold text-gray-800 hover:text-gray-600 ">
+            Flying High
+            </span>
+        </Link>
+            <span className="text-sm text-gray-500">Writing</span>
+            {children}
+        </div>
+    </div>
+  )
+}
+
+export default Author;
--- front-end_next/app/Components/Section1/section1.tsx
+++ front-end_next/app/Components/Section1/section1.tsx
@@ -1,28 +1,46 @@
-import React from 'react'
 import Image from 'next/image';
+import Link from 'next/link';
+import Author from '../Child/author';
 
 const section1 = () => {
-  return (
-    <section className='py-16'>
-        <div className='container mx-auto md:px20'>
-            <h1 className='font-bold text-4xl pb-12 text-center'>Trending</h1>
-            {Slide()}
-        </div>
-    </section>
-  )
+    return (
+        <section className='py-16'>
+            <div className='container mx-auto md:px20'>
+                <h1 className='font-bold text-4xl pb-12 text-center'>Star Blog</h1>
+                {Slide()}
+            </div>
+        </section>
+    )
 }
 
 export default section1;
 
 const Slide = () => {
     return (
-      <div className='grid md:grid-cols-2'>
-        <div className='image'>
-        <Image src={'/images/img1.jpg'} width={600} height={600} alt='Picture Blog' />
-        </div>
-        <div className='info'>
+        <div className='grid md:grid-cols-2'>
+            <div className='image'>
+                <Link href={'/'}>
+                    <Image src={'/images/img1.jpg'} width={600} height={600} alt='Picture Blog' />
+                </Link>
+            </div>
+            <div className='info flex justify-center flex-col'>
+                <div className='cat'>
+                    <Link href={'/'}>
+                        <span className='text-orange-600 hover:text-orange-800'>Reflexões Espirituais</span>
+                    </Link>
+                </div>
+                <div className='title'>
+                    <Link href={'/'}>
+                        <p className='text-3xl md:text-6xl font-bold text-gray-800 hover:text-gray-600'>You can be better every day if you let it.</p>
+                    </Link>
+                </div>
+                <p className='text-gray-500 py-3'>
+                even the almighty pointing unchecked over the blind texts is an almost unorthographic life one day however a small line of blind text by the name of Lorem ipsum decided to depart for the distant world of grammar
+                </p>
+                <Author>
 
+                </Author>
+            </div>
         </div>
-      </div>  
     )
 }
