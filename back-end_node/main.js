e6244225cf03d39876e9ed6987b5d67834eda3c6
Micael Santana <micaelsantana.dev@gmail.com>
2023-09-12 22:07:32 -0300

    install pg and express back-end and update url database .env

--- back-end_node/main.js
+++ back-end_node/main.js
@@ -1,14 +1,28 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
+const express = require("express");
+const { Pool } = require("pg");
 const app = express();
 const PORT = process.env.PORT;
+const DATABASE_URL = process.env.DATABASE_URL;
 
-app.get('/', (req, res) => {
-    let mensagem = {mensagem: 'Boa noite'};
-    res.json(mensagem);
+const pool = new Pool({
+  connectionString: DATABASE_URL,
+});
+
+app.get("/", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW() as current_time");
+    const current_time = result.rows[0].current_time;
+    res.json({ mensagem: "Boa noite", current_time });
+  } catch (error) {
+    console.error("Erro ao se conectar ao banco de dados:", error);
+    res.status(400).json({ error: "Erro ao se conectar ao banco de dados" });
+  }
 });
 
 app.listen(PORT, () => {
-    console.log(`Servidor iniciado em localhost no dia ${Date()} na porta ${PORT}`);
+  console.log(
+    `Servidor iniciado em localhost no dia ${Date()} na porta ${PORT}`
+  );
 });
