2cc59097aa9d68ff33f773b687c0e7ce6bedf378
Micael Santana <micaelsantana.dev@gmail.com>
2023-09-18 23:32:59 -0300

    update back

--- back-end_node/main.js
+++ back-end_node/main.js
@@ -11,18 +11,12 @@ const pool = new Pool({
 });
 
 app.get("/", async (req, res) => {
-  try {
-    const result = await pool.query("SELECT NOW() as current_time");
-    const current_time = result.rows[0].current_time;
-    res.json({ mensagem: "Boa noite", current_time });
-  } catch (error) {
-    console.error("Erro ao se conectar ao banco de dados:", error);
-    res.status(400).json({ error: "Erro ao se conectar ao banco de dados" });
-  }
+  let mensagem = { mensagem: 'Seja bem vindo' };
+  res.json(mensagem);
 });
 
 app.listen(PORT, () => {
   console.log(
-    `Servidor iniciado em localhost no dia ${Date()} na porta ${PORT}`
+    `Servidor iniciado em localhost no dia ${Date()}`
   );
 });
