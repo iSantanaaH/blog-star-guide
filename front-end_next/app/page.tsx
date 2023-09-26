0b6f7a22edf73e280b1c152b66f1121efd6ee4b5
Micael Santana <micaelsantana.dev@gmail.com>
2023-09-25 21:10:03 -0300

    update format main to add headerHome

--- front-end_next/app/page.tsx
+++ front-end_next/app/page.tsx
@@ -3,12 +3,12 @@ import Section1 from './Components/Section1/sectionIntroBlogSlide';
 import SectionPosts from './Components/Section2/sectionPosts';
 import MostPopular from './Components/MostPopular/sectionMostPopular';
 import OtherPosts from './Components/SectionOthersPosts/othersPosts';
-import ComponentFooter from './Components/Footer/footer';
-
+import HeaderHome from './Components/HeaderHome/headerHome';
 
 export default function Home() {
   return (
     <Format>
+      <HeaderHome />
       <Section1></Section1>
       <SectionPosts></SectionPosts>
       <MostPopular></MostPopular>
