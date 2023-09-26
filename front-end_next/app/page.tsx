ea6ef1bf0fa70b06d613d20d0c7bda6b3dabfeb8
Micael Santana <micaelsantana.dev@gmail.com>
2023-09-26 20:07:58 -0300

    Use fuction AuthProvider in AuthContext.tsx

--- front-end_next/app/page.tsx
+++ front-end_next/app/page.tsx
@@ -1,18 +1,21 @@
-import Format from '@/layout/format';
-import Section1 from './Components/Section1/sectionIntroBlogSlide';
-import SectionPosts from './Components/Section2/sectionPosts';
-import MostPopular from './Components/MostPopular/sectionMostPopular';
-import OtherPosts from './Components/SectionOthersPosts/othersPosts';
-import HeaderHome from './Components/HeaderHome/headerHome';
+import Format from "@/layout/format";
+import Section1 from "./Components/Section1/sectionIntroBlogSlide";
+import SectionPosts from "./Components/Section2/sectionPosts";
+import MostPopular from "./Components/MostPopular/sectionMostPopular";
+import OtherPosts from "./Components/SectionOthersPosts/othersPosts";
+import HeaderHome from "./Components/HeaderHome/headerHome";
+import { AuthProvider } from "./contexts/AuthContext";
 
 export default function Home() {
   return (
     <Format>
-      <HeaderHome />
-      <Section1></Section1>
-      <SectionPosts></SectionPosts>
-      <MostPopular></MostPopular>
-      <OtherPosts></OtherPosts>
+      <AuthProvider>
+        <HeaderHome />
+        <Section1></Section1>
+        <SectionPosts></SectionPosts>
+        <MostPopular></MostPopular>
+        <OtherPosts></OtherPosts>
+      </AuthProvider>
     </Format>
-  )
-};
+  );
+}
