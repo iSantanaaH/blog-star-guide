c277c8fdad11247c0082042a0b82133c3c0b5f80
Micael Santana <micaelsantana.dev@gmail.com>
2023-09-26 22:02:39 -0300

    removed attribute action and created function HandleSubmit to do req in back-end

--- front-end_next/app/pages/login/page.tsx
+++ front-end_next/app/pages/login/page.tsx
@@ -3,14 +3,54 @@ import Format from "@/layout/formatPages";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { SyntheticEvent, useRef } from "react";
+import axios from "axios";
 
 function FormLoginUser() {
+  const refFormLogin = useRef<HTMLFormElement | null>(null);
+
+  async function handleSubmit(event: SyntheticEvent) {
+    event.preventDefault();
+
+    try {
+      const formData = new FormData(event.currentTarget as HTMLFormElement);
+
+      const formDataObject: Record<string, any> = {};
+      formData.forEach((value, key) => {
+        formDataObject[key] = value;
+      });
+
+      const response = await axios.post(
+        "http://localhost:3333/login",
+        formDataObject,
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+        }
+      );
+
+      if (response.status === 200) {
+        console.log(response.data.email)
+        console.log(response.data.password)
+        refFormLogin.current?.reset();
+
+        setTimeout(() => {
+          window.location.href = "/";
+        }, 3000);
+      }
+    } catch (error: any) {
+      console.error("Erro ao registrar usuário:", error.message);
+    }
+  }
+
   return (
     <Format>
       <section className="flex flex-col justify-center items-center">
         <Form
-          action="http://localhost:3333/"
-          method="post"
+          onSubmit={handleSubmit}
+          ref={refFormLogin}
+          method="POST"
           className="flex flex-col justify-center items-center mt-32 py-5 bg-slate-200 rounded-2xl"
         >
           <div>
@@ -18,12 +58,12 @@ function FormLoginUser() {
           </div>
           <Form.Group className="mb-3 sm:px-8" controlId="formBasicEmail">
             <Form.Label>Email</Form.Label>
-            <Form.Control type="email" placeholder="email" />
+            <Form.Control type="email" name="email" placeholder="email" required />
           </Form.Group>
 
           <Form.Group className="mb-3 sm:px-8" controlId="formBasicPassword">
             <Form.Label>Senha</Form.Label>
-            <Form.Control type="password" placeholder="senha" />
+            <Form.Control type="password" name="password" placeholder="senha" required />
           </Form.Group>
           <Button variant="primary" type="submit">
             Logar
