ab69537632bad474511013d35d0032f4a3b95de0
Micael Santana <micaelsantana.dev@gmail.com>
2023-09-26 22:00:46 -0300

    add req for back-end in AuthContext.tsx

--- front-end_next/app/contexts/AuthContext.tsx
+++ front-end_next/app/contexts/AuthContext.tsx
@@ -1,17 +1,40 @@
-import { createContext } from "react";
+"use client"
+import { createContext, useState } from "react";
+import axios from "axios";
 
 type AuthContextType = {
-    isAuthenticated: boolean;
-}
+  isAuthenticated: boolean;
+};
+
+type SignInData = {
+  email: string;
+  password: string;
+};
 
 export const AuthContext = createContext({} as AuthContextType);
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
-    const isAuthenticated = false;
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  async function signIn({ email, password }: SignInData) {
+    try {
+      const response = await axios.post("http://localhost:3333/login", {
+        email,
+        password,
+      });
+
+      const TOKEN = response.data.TOKEN;
+
+      setIsAuthenticated(true);
+    } catch (error) {
+      console.error("Erro ao fazer login", error);
+      setIsAuthenticated(false);
+    }
+  }
 
   return (
-    <AuthContext.Provider value={{isAuthenticated}}>
-        {children}
+    <AuthContext.Provider value={{ isAuthenticated }}>
+      {children}
     </AuthContext.Provider>
-  ) 
+  );
 }
