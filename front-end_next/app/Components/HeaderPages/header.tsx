0561e83ba1d4a1c3f83f73acb2c2f539b8d7a371
Micael Santana <micaelsantana.dev@gmail.com>
2023-09-30 19:11:03 -0300

    added reponsive for small screens

--- front-end_next/app/Components/HeaderPages/header.tsx
+++ front-end_next/app/Components/HeaderPages/header.tsx
@@ -5,19 +5,19 @@ import Link from "next/link";
 
 const header = () => {
   return (
-    <header className="bg-slate-100 z-10 w-full">
+    <header className="bg-slate-100 z-10 w-full sml:w-full sml:overflow-hidden sml:pb-4 sm639:pb-4">
       <div className="xl:container xl:mx-auto flex flex-col items-center sm:flex-row sm:justify-between text-center">
         <div className="md:flex-none w-96 order-2 sm:order-1 flex justify-center py-4 sm:py-0">
           <input type="text" className="input-text" placeholder="Search..." />
         </div>
-        <div className="shrink w-80 sm:order-2">
+        <div className="shrink w-80 sm:order-2 sml:pt-8 sm639:pt-8">
           <Link href={"/"} className="font-bold uppercase text-3xl">
             A estrela guia
           </Link>
         </div>
-        <div className="w96 order-3 flex justify-center">
+        <div className="w-96 order-3 flex justify-center">
           <div className="flex gap-6 sm:mr-4">
-            {/* <Link href={"/"}>
+            <Link href={"/"}>
               <ImFacebook color="#888888" />
             </Link>
             <Link href={"/"}>
@@ -25,7 +25,7 @@ const header = () => {
             </Link>
             <Link href={"/"}>
               <ImYoutube color="#888888" />
-            </Link> */}
+            </Link>
             <Link href={"/"}>
               <FaUserCircle color="#888888" />
             </Link>
