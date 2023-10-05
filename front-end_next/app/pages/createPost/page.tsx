0c536182a05cf988c01854b2d154510262fb3aaf
Micael Santana <micaelsantana.dev@gmail.com>
2023-10-05 00:28:25 -0300

    change page createpost

--- front-end_next/app/pages/createPost/page.tsx
+++ front-end_next/app/pages/createPost/page.tsx
@@ -1,59 +1,107 @@
 "use client";
 import Format from "@/layout/format";
+import axios from "axios";
+import { useRef, SyntheticEvent } from "react";
+import { ToastContainer, toast } from "react-toastify";
 
 const CreatePost = () => {
+  const formRef = useRef<HTMLFormElement | null>(null);
+
+  async function handleSubmit(event: SyntheticEvent) {
+    event.preventDefault();
+
+    try {
+      const formData = new FormData(event.currentTarget as HTMLFormElement);
+
+      const formDataObject: Record<string, any> = {};
+      formData.forEach((value, key) => {
+        formDataObject[key] = value;
+      });
+
+      const response = await axios.post(
+        "http://localhost:3333/createpost",
+        formDataObject,
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+        }
+      );
+
+      if (response.status === 200) {
+        formRef.current?.reset();
+
+        const notifySucessCreatePost = () => {
+          toast("Post criado com sucesso!");
+        };
+
+        notifySucessCreatePost();
+
+        console.log(response.data);
+      }
+    } catch (error: any) {
+      console.error("Alguns campos estão errados", error.message);
+    }
+  }
+
   return (
     <Format>
       <section className="container py-16 mx-auto md:px-2 md:w-10/12">
-        <div className="flex justify-center py-10">
-          <h1 className="font-bold text-4xl text-center pb-5">
-            Criar Novo Post
-          </h1>
+        <div className="containerNotify">
+          <ToastContainer />
         </div>
 
-        <div className="flex justify-center">
-          <div className="pb-8 flex justify-between items-center">
-            <label htmlFor="titulo" className="text-lg font-semibold mx-5">
-              Titulo do post
-            </label>
-            <input
-              type="text"
-              id="titulo"
-              className="input-post mt-2 row-span-2"
-              placeholder="Titulo do post"
-            />
+        <form method="POST" onSubmit={handleSubmit} ref={formRef}>
+          <div className="flex justify-center py-10">
+            <h1 className="font-bold text-4xl text-center pb-5">
+              Criar Novo Post
+            </h1>
           </div>
-        </div>
 
-        <div className="container flex justify-center">
-          <div className="w-2/4">
-            <label
-              htmlFor="descricao"
-              className="text-lg font-semibold flex pb-4"
-            >
-              Descrição do post
-            </label>
-            <textarea className="mt-2 w-ful sm:w-full" id="default"></textarea>
+          <div className="flex justify-center">
+            <div className="pb-8 flex justify-between items-center">
+              <label htmlFor="titule" className="text-lg font-semibold mx-5">
+                Titulo do post
+              </label>
+              <input
+                type="text"
+                id="titule"
+                name="titule"
+                className="input-post mt-2 row-span-2"
+                placeholder="Titulo do post"
+              />
+            </div>
           </div>
-        </div>
 
-        <div className="flex justify-center py-10">
-          <div className="flex items-center flex-col">
-            <label htmlFor="imagem" className="text-lg pb-3">
-              Importar imagem
-            </label>
-            <input type="file" id="imagem" className="mt-2" />
+          <div className="container flex justify-center">
+            <div className="w-2/4">
+              <label
+                htmlFor="description"
+                className="text-lg font-semibold flex pb-4"
+              >
+                Descrição do post
+              </label>
+              <textarea
+                className="mt-2 w-ful sm:w-full"
+                id="description"
+                name="content"
+              ></textarea>
+            </div>
           </div>
-        </div>
 
-        <div className="flex justify-center py-5">
-          <button
-            type="submit"
-            className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700"
-          >
-            Criar Post
-          </button>
-        </div>
+          <div className="flex justify-center py-10">
+            <div className="flex items-center flex-col">
+              <label className="text-lg pb-3">Importar imagem</label>
+              <input type="file" id="image" className="mt-2" />
+            </div>
+          </div>
+
+          <div className="flex justify-center py-5">
+            <button type="submit" className="button-form">
+              Criar Post
+            </button>
+          </div>
+        </form>
       </section>
     </Format>
   );
