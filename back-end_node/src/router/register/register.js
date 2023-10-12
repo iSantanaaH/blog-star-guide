ec0bef0d1c722e54092ab5ae5c59d27f1b3cbaff
Micael Santana <micaelsantana.dev@gmail.com>
2023-10-12 01:29:04 -0300

    create router register

---
+++ back-end_node/src/router/register/register.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = express.Router();
+
+/* Database */
+const pool = require("../../config/database/database");
+
+router.post("/register", async (req, res) => {
+  try {
+    let { name, surname, email, password, phone, birthday } = req.body;
+
+    const checkUserEmailQuery = `SELECT * FROM users WHERE email = $1`;
+    const checkUserEmailResult = await pool.query(checkUserEmailQuery, [email]);
+
+    if (checkUserEmailResult.rows.length > 0) {
+      return res.status(400).json({ error: "Este email já está cadastrado" });
+    }
+
+    const insertUserQuery = `
+        INSER INTO users (name, surname, email, password, phone, birthday) VALUES ($1, $2, $3, $4, $5, $6);
+        `;
+
+    const values = [name, surname, email, password, phone, birthday];
+    await pool.query(insertUserQuery, values);
+
+    res.status(200).json({ mensagem: "Usuário cadastrado com sucesso" });
+  } catch (error) {
+    console.error("Erro ao registrar usuário", error.menssage);
+    res.status(400).json({ erro: "Erro ao cadastrar o usuário" });
+  }
+});
