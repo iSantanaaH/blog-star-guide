30cbac0cca82858067af8ad071c084cb0bccebb0
Micael Santana <micaelsantana.dev@gmail.com>
2023-10-12 03:18:27 -0300

    remove created inside server.js, import userTableSetup and calling variable inside async

--- back-end_node/main.js
+++ back-end_node/main.js
@@ -14,19 +14,14 @@ const routerCreatePost = require("./src/router/createPost/createPost");
 /* Database */
 const pool = require("./src/config/database/database");
 
+/* Setup Database */
+const userTableSetup = require("./src/config/databaseSetup/userTable/userTableSetup");
+
 /* Middleware */
 const authenticationMiddleware = require("./src/config/middleware/authentication/authenticationMiddleware");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: "http://localhost:3000" }));
-
-const checkTableUsersQuery = `
-  SELECT EXISTS (
-    SELECT 1
-    FROM information_schema.tables
-    WHERE table_name = 'users'
-  )
-`;
 
 const checkTablePostsQuery = `
   SELECT EXISTS (
@@ -45,6 +40,10 @@ const checkTableUserPermission = `
 `;
 
 (async () => {
+  await userTableSetup();
+});
+
+(async () => {
   const client = await pool.connect();
   try {
     const resultUserQuery = await client.query(checkTableUsersQuery);
@@ -52,25 +51,8 @@ const checkTableUserPermission = `
     const resultUserPermissionQuery = await client.query(
       checkTableUserPermission
     );
-    const tableUserExists = resultUserQuery.rows[0].exists;
     const tablePostsExists = resultPostsQuery.rows[0].exists;
     const tableUserPermission = resultUserPermissionQuery.rows[0].exists;
-
-    if (!tableUserExists) {
-      const createTableUserQuery = `
-      CREATE TABLE IF NOT EXISTS "users" (
-        id SERIAL PRIMARY KEY,
-        name VARCHAR(255) NOT NULL,
-        surname VARCHAR(255) NOT NULL,
-        email VARCHAR(255) NOT NULL,
-        phone VARCHAR(20) NOT NULL,
-        password VARCHAR(255) NOT NULL,
-        birthday TIMESTAMP NOT NULL,
-        user_permission_id INT
-      )
-    `;
-      await client.query(createTableUserQuery);
-    }
 
     if (!tablePostsExists) {
       const createTablePostsQuery = `
