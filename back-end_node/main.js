5f6fe7c0b9a6da82a28b650fd3012010dc3c297a
Micael Santana <micaelsantana.dev@gmail.com>
2023-10-12 02:55:15 -0300

    remove imports unsed, remove router /createpost inside server.js and import app.use

--- back-end_node/main.js
+++ back-end_node/main.js
@@ -1,22 +1,21 @@
 require("dotenv").config();
 
 const express = require("express");
-const jwt = require("jsonwebtoken");
 const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT;
-const secretKey = process.env.JWT_SECRET;
 
 /* Router */
 const routerRegister = require("./src/router/register/register");
 const routerLogin = require("./src/router/login/login");
 const routerLatestPost = require("./src/router/latestPost/latestPost");
+const routerCreatePost = require("./src/router/createPost/createPost");
 
 /* Database */
 const pool = require("./src/config/database/database");
 
 /* Middleware */
-const authenticationMiddleware = require("./src/config/middleware/authenticationMiddleware");
+const authenticationMiddleware = require("./src/config/middleware/authentication/authenticationMiddleware");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: "http://localhost:3000" }));
@@ -104,59 +103,7 @@ const checkTableUserPermission = `
 app.use("/register", routerRegister);
 app.use("/login", routerLogin);
 app.use("/latestpost", routerLatestPost);
-
-app.post("/createpost", authenticationMiddleware, async (req, res) => {
-  try {
-    const token = req.headers.authorization;
-    console.log(token);
-
-    if (!token) {
-      return res
-        .status(401)
-        .json({ error: "Usuário não tem permissão para criar um post" });
-    }
-    const decodedToken = jwt.verify(token, secretKey);
-    const userId = decodedToken.userId;
-    const checkUserPermissionQuery =
-      "SELECT user_permission_id FROM users WHERE id = $1";
-    const permissionResult = await pool.query(checkUserPermissionQuery, [
-      userId,
-    ]);
-
-    if (permissionResult.rows.length === 0) {
-      return res.status(401).json({ error: "Usuário não encontrado" });
-    }
-
-    const userPermissionId = permissionResult.rows[0].user_permission_id;
-
-    if (userPermissionId !== 1) {
-      return res
-        .status(403)
-        .json({ error: "Usuário não tem permissão para criar um post" });
-    }
-
-    let { titule, content } = req.body;
-
-    const createNewPostQuery = `
-    INSERT INTO posts(titule, content, user_id, date_created) VALUES ($1, $2, $3, NOW()) RETURNING titule, content, date_created
-    `;
-
-    const values = [titule, content, userId];
-    const newPostResult = await pool.query(createNewPostQuery, values);
-
-    if (newPostResult.rows.length === 0) {
-      return res.status(400).json({ error: "Erro ao criar a postagem" });
-    }
-
-    const newPost = newPostResult.rows[0];
-    res
-      .status(200)
-      .json({ mensagem: "Postagem criada com sucesso!", post: newPost });
-  } catch (error) {
-    console.error("Erro ao criar a postagem", error.message);
-    res.status(400).json({ error: "Erro ao criar a postagem" });
-  }
-});
+app.use("/createpost", routerCreatePost);
 
 app.listen(PORT, () => {
   console.log(`Servidor iniciado em localhost no dia ${Date()}`);
