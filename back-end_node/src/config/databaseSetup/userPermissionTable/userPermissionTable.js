31c0329edca77b8653e392d3a5b418e7d4dbca53
Micael Santana <micaelsantana.dev@gmail.com>
2023-10-12 03:48:07 -0300

    create userPermissionTable.js

---
+++ back-end_node/src/config/databaseSetup/userPermissionTable/userPermissionTable.js
@@ -0,0 +1,34 @@
+const pool = require("../../database/database");
+
+const checkTableUserPermission = `
+  SELECT EXISTS (
+    SELECT 1
+    FROM information_schema.tables
+    WHERE table_name = 'user_permission'
+  )
+`;
+
+async function setupTableUserPermission() {
+  const client = await pool.connect();
+  try {
+    const resultUserPermissionQuery = await client.query(
+      checkTableUserPermission
+    );
+    const tableUserPermission = resultUserPermissionQuery.rows[0].exists;
+
+    if (!tableUserPermission) {
+      const createTableUserPermissionQuery = `
+                CREATE TABLE IF NOT EXISTS "user_permission" (
+                  id SERIAL PRIMARY KEY
+                )
+              `;
+      await client.query(createTableUserPermissionQuery);
+    }
+  } catch (error) {
+    console.error("Erro ao criar a tabela user_permission");
+  } finally {
+    client.release();
+  }
+}
+
+module.exports = setupTableUserPermission;
