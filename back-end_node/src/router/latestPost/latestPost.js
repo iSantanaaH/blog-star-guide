4903e13878cc0840b8fa49002e6df1f191aa1f0d
Micael Santana <micaelsantana.dev@gmail.com>
2023-10-12 02:55:48 -0300

    added config to database

--- back-end_node/src/router/latestPost/latestPost.js
+++ back-end_node/src/router/latestPost/latestPost.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+/* Database */
+const pool = require("../../config/database/database");
+
 router.get("", async (req, res) => {
   try {
     const latestPostQuery = `
