a9490333adb4b4fdf2c70b73518ca9bd8e1ce250
Micael Santana <micaelsantana.dev@gmail.com>
2023-10-12 03:29:29 -0300

    remove creating table inside server.js and changed calling error function async to execute variables create tables

--- back-end_node/main.js
+++ back-end_node/main.js
@@ -16,20 +16,13 @@ const pool = require("./src/config/database/database");
 
 /* Setup Database */
 const userTableSetup = require("./src/config/databaseSetup/userTable/userTableSetup");
+const postsTableSetup = require("./src/config/databaseSetup/postsTable/postTable");
 
 /* Middleware */
 const authenticationMiddleware = require("./src/config/middleware/authentication/authenticationMiddleware");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: "http://localhost:3000" }));
-
-const checkTablePostsQuery = `
-  SELECT EXISTS (
-    SELECT 1
-    FROM information_schema.tables
-    WHERE table_name = 'posts'
-  )
-`;
 
 const checkTableUserPermission = `
   SELECT EXISTS (
@@ -41,46 +34,29 @@ const checkTableUserPermission = `
 
 (async () => {
   await userTableSetup();
-});
-
-(async () => {
-  const client = await pool.connect();
-  try {
-    const resultUserQuery = await client.query(checkTableUsersQuery);
-    const resultPostsQuery = await client.query(checkTablePostsQuery);
-    const resultUserPermissionQuery = await client.query(
-      checkTableUserPermission
-    );
-    const tablePostsExists = resultPostsQuery.rows[0].exists;
-    const tableUserPermission = resultUserPermissionQuery.rows[0].exists;
-
-    if (!tablePostsExists) {
-      const createTablePostsQuery = `
-    CREATE TABLE IF NOT EXISTS "posts" (
-      id SERIAL PRIMARY KEY,
-      titule VARCHAR(255) NOT NULL,
-      content VARCHAR(255) NOT NULL,
-      date_created TIMESTAMP,
-      data_change TIMESTAMP,
-      comments VARCHAR(255),
-      user_id INT NOT NULL
-    )
-    `;
-      await client.query(createTablePostsQuery);
-    }
-
-    if (!tableUserPermission) {
-      const createTableUserPermissionQuery = `
-        CREATE TABLE IF NOT EXISTS "user_permission" (
-          id SERIAL PRIMARY KEY
-        )
-      `;
-      await client.query(createTableUserPermissionQuery);
-    }
-  } finally {
-    client.release();
-  }
+  await postsTableSetup();
 })();
+
+// (async () => {
+//   const client = await pool.connect();
+//   try {
+//     const resultUserPermissionQuery = await client.query(
+//       checkTableUserPermission
+//     );
+//     const tableUserPermission = resultUserPermissionQuery.rows[0].exists;
+
+//     if (!tableUserPermission) {
+//       const createTableUserPermissionQuery = `
+//         CREATE TABLE IF NOT EXISTS "user_permission" (
+//           id SERIAL PRIMARY KEY
+//         )
+//       `;
+//       await client.query(createTableUserPermissionQuery);
+//     }
+//   } finally {
+//     client.release();
+//   }
+// })();
 
 app.use("/register", routerRegister);
 app.use("/login", routerLogin);
