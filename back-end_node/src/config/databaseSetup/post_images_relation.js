7633fd0cc86f4a790eb8e48f2c4fbeea7fffbd1d
Micael Santana <micaelsantana.dev@gmail.com>
2023-10-17 20:51:34 -0300

    create table post_images_relation

---
+++ back-end_node/src/config/databaseSetup/post_images_relation.js
@@ -0,0 +1,57 @@
+const pool = require("../database/database");
+
+const checkTablePostsQuery = `
+  SELECT EXISTS (
+    SELECT 1
+    FROM information_schema.tables
+    WHERE table_name = 'posts'
+  )
+`;
+
+const checkTableImagesQuery = `
+  SELECT EXISTS (
+    SELECT 1
+    FROM information_schema.tables
+    WHERE table_name = 'images'
+  )
+`;
+
+const checkTablePostImagesRelations = `
+  SELECT EXISTS (
+    SELECT 1
+    FROM information_schema.tables
+    WHERE table_name = 'post_images_relation'
+  )
+`;
+
+async function setupPostsImagesRelations() {
+  const client = await pool.connect();
+  try {
+    const resultPostsQuery = await client.query(checkTablePostsQuery);
+    const resultImagesQuery = await client.query(checkTableImagesQuery);
+    const resultPostImagesRelationsQuery = await client.query(
+      checkTablePostImagesRelations
+    );
+
+    const tableImagesExists = resultImagesQuery.rows[0].exists;
+    const tablePostsExists = resultPostsQuery.rows[0].exists;
+    const tablePostImagesRelations =
+      resultPostImagesRelationsQuery.rows[0].exists;
+
+    if (!tablePostImagesRelations) {
+      if (tableImagesExists && tablePostsExists) {
+        const createTablePostsImagesRelations = `
+              CREATE TABLE IF NOT EXISTS "post_images_relation" (
+                  post_id INT NOT NULL REFERENCES posts(id),
+                  image_id INT NOT NULL REFERENCES images(id)
+              );
+              `;
+        await client.query(createTablePostsImagesRelations);
+      }
+    }
+  } catch (error) {
+    console.error(`Erro ao criar a tabela PostsImages: ${error.message}`);
+  }
+}
+
+module.exports = setupPostsImagesRelations;
