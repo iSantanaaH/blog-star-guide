c2aba5dea12d0536806f39065402121b49172086
Micael Santana <micaelsantana.dev@gmail.com>
2023-10-19 16:25:39 -0300

    update createpost

--- back-end_node/src/router/createPost.js
+++ back-end_node/src/router/createPost.js
@@ -1,54 +1,38 @@
-/* Database */
-const pool = require("../config/database/database");
-
-/* Outra Importações */
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const router = express.Router();
 const secretKey = process.env.JWT_SECRET;
 const multer = require("multer");
-const path = require("path");
+const upload = multer({ dest: "uploads" });
 const fs = require("fs");
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, "uploads/"));
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname));
-  },
-});
-
-const upload = multer({ storage: storage });
+/* Database */
+const pool = require("../config/database/database");
 
 router.post("", upload.single("image"), async (req, res) => {
   try {
+    const image = req.file;
     const token = req.headers.authorization;
+
+    const tempFilePath = image.destination + "/" + image.filename;
+    const originalFileName = image.originalname;
+    const targetDirectory = "uploads";
+    const targetPath = `${targetDirectory}/${originalFileName}`;
+
+    fs.rename(tempFilePath, targetPath, (err) => {
+      if (err) {
+        console.error("Erro ao salvar a imagem:", err);
+        res.status(500).json({ error: "Erro ao salvar a imagem" });
+      } else {
+        console.log("A imagem foi salva com sucesso em:", targetPath);
+      }
+    });
 
     if (!token) {
       return res
         .status(401)
         .json({ error: "Usuário não tem permissão para criar um post" });
     }
-
-    if (req.file) {
-      const imagePath = req.file.path;
-
-      const saveImageQuery = `INSERT INTO images (name, image_path) VALUES ($1, $2) RETURNING id`;
-      const imageValues = [req.file.filename, imagePath];
-      const imageResult = await pool.query(saveImageQuery, imageValues);
-
-      if (imageResult.rows.length === 0) {
-        return res.status(400).json({ error: "Erro ao salvar a imamge" });
-      }
-
-      const imageId = imageResult.rows[0].id;
-
-      const linkImagePostQuery = `INSERT INTO post_images_relation (post_id, image_id) VALUES ($1, $2)`;
-      const linkValues = [newPost.id, imageId];
-      await pool.query(linkImagePostQuery, linkValues);
-    }
-
     const decodedToken = jwt.verify(token, secretKey);
     const userId = decodedToken.userId;
     const checkUserPermissionQuery =
@@ -69,15 +53,10 @@ router.post("", upload.single("image"), async (req, res) => {
         .json({ error: "Usuário não tem permissão para criar um post" });
     }
 
-    let { title, content, image } = req.body;
-
-    if (!title || !content || !image) {
-      return res.status(400).json({ error: "Por favor, preencha todos os campos obrigatórios" });
-    }
-    console.log(title);
+    let { title, content } = req.body;
 
     const createNewPostQuery = `
-        INSERT INTO posts(title, content, user_id, date_created) VALUES ($1, $2, $3, NOW()) RETURNING title, content, date_created, user_id
+        INSERT INTO posts(title, content, user_id, date_created) VALUES ($1, $2, $3, NOW()) RETURNING title, content, date_created
         `;
 
     const values = [title, content, userId];
