7994ab744f12f5abe1d6097f292b097ce90da541
Micael Santana <micaelsantana.dev@gmail.com>
2023-10-18 21:38:40 -0300

    change rout createpost to create post, but still need to upload the image

--- back-end_node/src/router/createPost.js
+++ back-end_node/src/router/createPost.js
@@ -1,21 +1,54 @@
+/* Database */
+const pool = require("../config/database/database");
+
+/* Outra Importações */
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const router = express.Router();
 const secretKey = process.env.JWT_SECRET;
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
 
-/* Database */
-const pool = require("../config/database/database");
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, "uploads/"));
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
 
-router.post("", async (req, res) => {
+const upload = multer({ storage: storage });
+
+router.post("", upload.single("image"), async (req, res) => {
   try {
     const token = req.headers.authorization;
-    console.log(token);
 
     if (!token) {
       return res
         .status(401)
         .json({ error: "Usuário não tem permissão para criar um post" });
     }
+
+    if (req.file) {
+      const imagePath = req.file.path;
+
+      const saveImageQuery = `INSERT INTO images (name, image_path) VALUES ($1, $2) RETURNING id`;
+      const imageValues = [req.file.filename, imagePath];
+      const imageResult = await pool.query(saveImageQuery, imageValues);
+
+      if (imageResult.rows.length === 0) {
+        return res.status(400).json({ error: "Erro ao salvar a imamge" });
+      }
+
+      const imageId = imageResult.rows[0].id;
+
+      const linkImagePostQuery = `INSERT INTO post_images_relation (post_id, image_id) VALUES ($1, $2)`;
+      const linkValues = [newPost.id, imageId];
+      await pool.query(linkImagePostQuery, linkValues);
+    }
+
     const decodedToken = jwt.verify(token, secretKey);
     const userId = decodedToken.userId;
     const checkUserPermissionQuery =
@@ -36,13 +69,18 @@ router.post("", async (req, res) => {
         .json({ error: "Usuário não tem permissão para criar um post" });
     }
 
-    let { titule, content } = req.body;
+    let { title, content, image } = req.body;
+
+    if (!title || !content || !image) {
+      return res.status(400).json({ error: "Por favor, preencha todos os campos obrigatórios" });
+    }
+    console.log(title);
 
     const createNewPostQuery = `
-        INSERT INTO posts(titule, content, user_id, date_created) VALUES ($1, $2, $3, NOW()) RETURNING titule, content, date_created
+        INSERT INTO posts(title, content, user_id, date_created) VALUES ($1, $2, $3, NOW()) RETURNING title, content, date_created, user_id
         `;
 
-    const values = [titule, content, userId];
+    const values = [title, content, userId];
     const newPostResult = await pool.query(createNewPostQuery, values);
 
     if (newPostResult.rows.length === 0) {
