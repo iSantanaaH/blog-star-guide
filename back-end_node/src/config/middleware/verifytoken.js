a78cce2cae1bf39f862b5ba8e104dcb1d50f9694
Micael Santana <micaelsantana.dev@gmail.com>
2023-10-18 21:37:05 -0300

    delete authenticationMiddleware.js and verify token

--- back-end_node/src/config/middleware/verifytoken.js
+++
@@ -1,8 +0,0 @@
-function verifyToken(req, res) {
-    const token = req.headers.authorization;
-
-    if (!token) {
-        return res.status(401).json({ error: "Token não fornecido" });
-    }
-
-}
