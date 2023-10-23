d07c3d0103b740940d25637492e0ef3e256384de
Micael Santana <micaelsantana.dev@gmail.com>
2023-10-23 13:19:37 -0300

    update page post

--- front-end_next/app/pages/post/page.tsx
+++ front-end_next/app/pages/post/page.tsx
@@ -1,9 +1,32 @@
 import Author from "@/app/Components/Child/author";
 import Related from "@/app/Components/Child/related";
 import Format from "@/layout/format";
+import axios from "axios";
 import Image from "next/image";
 
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
 function PostSectionMostRecent() {
+  const router = useRouter();
+  const { id } = router.query;
+  const [post, setPost] = useState({ title: "", content: "", date_created: Date, date_chenge: Date, comments: "", user_id: "" });
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      try {
+        if (id) {
+          const response = await axios.get(`http://localhost:3333/posts/${id}`)
+          const responseData = response.data;
+          setPost(responseData);
+        }
+      } catch (error: any) {
+        console.log("Erro ao buscar detalhes da postagem:", error.message);
+      }
+    }
+    fetchPost();
+  }, [id]);
+
   return (
     <Format>
       <section className="container mx-auto md:px-2 py-16 w-1/2">
@@ -15,11 +38,14 @@ function PostSectionMostRecent() {
 
         <div className="post py-10">
           <h1 className="font-bold text-4xl text-center pb-5">
-            Aqui, mergulhamos nos mistérios celestiais da inspiração e da
-            sabedoria.
-
+            {/* Aqui, mergulhamos nos mistérios celestiais da inspiração e da
+            sabedoria. */}
+            {post.title}
           </h1>
-            <p className="text-gray-500 text-xl text-center">Muito longe, atrás da palavra montanhas, voe, então voe alto</p>
+            <p className="text-gray-500 text-xl text-center">
+              {/* Muito longe, atrás da palavra montanhas, voe, então voe alto */}
+              {post.content}
+              </p>
 
             <div className="py-10">
                 <Image src={'/images/img1.png'} width={900} height={600} alt={''} />
