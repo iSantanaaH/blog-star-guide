2e8767e9e6de5394fe24b2c6678029496678bdba
Micael Santana <micaelsantana.dev@gmail.com>
2023-10-28 12:00:02 -0300

    change title

--- front-end_next/app/Components/Header/header.tsx
+++ front-end_next/app/Components/Header/header.tsx
@@ -80,7 +80,7 @@ const HeaderComponent = () => {
         </div>
         <div className="shrink w-80 sm:order-2 sm639:pt-4">
           <a href="/" className="font-bold uppercase text-3xl">
-            A estrela guia
+            estrela guia
           </a>
         </div>
         <div className="w96 order-3 flex justify-center sml:pb-4 sm639:pb-4">
