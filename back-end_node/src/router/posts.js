317271230c327881aa5b10968423c40bd7a47396
Micael Santana <micaelsantana.dev@gmail.com>
2023-10-30 13:01:24 -0300

    insert query to image in table images

--- back-end_node/src/router/posts.js
+++ back-end_node/src/router/posts.js
@@ -12,8 +12,15 @@ router.get("/:id", async (req, res) => {
         `;
     const { rows } = await pool.query(resultPostsQuery, [postId]);
 
+    const getImagePathQuery = `
+    SELECT images.image_path FROM images INNER JOIN post_images_relation ON images.id = post_images_relation.image_id WHERE post_images_relation.post_id = $1
+    `;
+    const imagePathResult = await pool.query(getImagePathQuery, [postId]);
+    const imagePath = imagePathResult.rows[0].image_path;
+    console.log(`O path da imagem é: ${imagePath}`);
+
     if (rows.length === 1) {
-      return res.status(200).json({ rows: rows[0] });
+      return res.status(200).json({ rows: rows[0], imagePath: imagePath });
     } else {
       return res.status(400).json({ error: "Post não encontrado" });
     }
