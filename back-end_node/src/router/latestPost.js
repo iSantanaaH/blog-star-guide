edd60fb54fe99b48c3842a9d5f974ead2450d4b1
Micael Santana <micaelsantana.dev@gmail.com>
2023-10-31 13:36:47 -0300

    change query to receive latestpost

--- back-end_node/src/router/latestPost.js
+++ back-end_node/src/router/latestPost.js
@@ -7,8 +7,13 @@ const pool = require("../config/database/database");
 router.get("", async (req, res) => {
   try {
     const latestPostQuery = `
-        SELECT * FROM posts ORDER BY date_created DESC
-        `;
+    SELECT posts.*, images.image_path
+    FROM posts
+    LEFT JOIN post_images_relation ON posts.id = post_images_relation.post_id
+    LEFT JOIN images ON post_images_relation.image_id = images.id
+    ORDER BY posts.date_created DESC
+  `;
+
     const latestPostResult = await pool.query(latestPostQuery);
 
     if (latestPostResult.rows.length === 0) {
@@ -19,6 +24,7 @@ router.get("", async (req, res) => {
     res.status(200).json(latestPost);
   } catch (error) {
     console.error("Erro ao obter o último post", error.message);
+    res.status(500).json({ error: "Erro ao obter o último post" });
   }
 });
 
