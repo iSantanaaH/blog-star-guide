4b7ccc3efbc1cbf30e3be5c604ee2957797bcd61
Micael Santana <micaelsantana.dev@gmail.com>
2023-10-31 20:38:54 -0300

    change to static Author

--- front-end_next/app/Components/Child/author.tsx
+++ front-end_next/app/Components/Child/author.tsx
@@ -8,38 +8,38 @@ type AuthorProps = {
 };
 
 const Author = ({ children }: AuthorProps) => {
-  const [userName, setUserName] = useState(null);
+  // const [userName, setUserName] = useState(null);
 
-  useEffect(() => {
-    const cookies = document.cookie.split(";");
-    let token = null;
+  // useEffect(() => {
+  //   const cookies = document.cookie.split(";");
+  //   let token = null;
 
-    for (let i = 0; i < cookies.length; i++) {
-      const cookie = cookies[i].trim();
+  //   for (let i = 0; i < cookies.length; i++) {
+  //     const cookie = cookies[i].trim();
 
-      if (cookie.startsWith("blogstarguide.token=")) {
-        token = cookie.substring("blogstarguide.token=".length, cookie.length);
-        break;
-      }
-    }
+  //     if (cookie.startsWith("blogstarguide.token=")) {
+  //       token = cookie.substring("blogstarguide.token=".length, cookie.length);
+  //       break;
+  //     }
+  //   }
 
-    if (token) {
-      try {
-        const decodedToken = JSON.parse(atob(token.split(".")[1]));
-        const user_permission_id = decodedToken.user_permission_id;
+  //   if (token) {
+  //     try {
+  //       const decodedToken = JSON.parse(atob(token.split(".")[1]));
+  //       const user_permission_id = decodedToken.user_permission_id;
 
-        if (!token || user_permission_id !== 1) {
-        } else {
-        }
+  //       if (!token || user_permission_id !== 1) {
+  //       } else {
+  //       }
 
-        if (token) {
-          const decodedToken = JSON.parse(atob(token.split(".")[1]));
-          const fullNameUser = decodedToken.fullName;
-          setUserName(fullNameUser);
-        }
-      } catch (error) {}
-    }
-  }, []);
+  //       if (token) {
+  //         const decodedToken = JSON.parse(atob(token.split(".")[1]));
+  //         const fullNameUser = decodedToken.fullName;
+  //         setUserName(fullNameUser);
+  //       }
+  //     } catch (error) {}
+  //   }
+  // }, []);
 
   return (
     <div className="author flex py-5">
@@ -56,6 +56,10 @@ const Author = ({ children }: AuthorProps) => {
 
       <div className="flex flex-col justify-center px-4">
         <Link href={"/"}>
+        <span className="text-md font-bold text-gray-800 hover:text-gray-600 ">Ana Karita</span>
+        </Link>
+        <span className="text-sm text-gray-500">...</span>
+        {/* <Link href={"/"}>
           
             {userName !== null ? (
               <>
@@ -74,7 +78,7 @@ const Author = ({ children }: AuthorProps) => {
           </>
         ) : (
           <span className="text-sm text-gray-500">...</span>
-        )}
+        )} */}
         {children}
       </div>
     </div>
