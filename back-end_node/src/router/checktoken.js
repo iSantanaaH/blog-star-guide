1c425f4a0b0650a8a3521aa53fe81678124b30cc
Micael Santana <micaelsantana.dev@gmail.com>
2023-11-22 23:28:12 -0300

    delete checkToken.js

--- back-end_node/src/router/checktoken.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-/* Middleware */
-const checkUserToken = require("../../config/middleware/verifyToken/verifytoken");
-
-router.post("/checktoken", checkUserToken, async (req, res) => {
-  res.status(200).json({ message: "Token válido" });
-});
